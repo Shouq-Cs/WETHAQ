30c8b9b1b7b766439186bb8cb5a5fe3bcbdf64d2
Shouq-Cs <shooqabdulsalam20@gmail.com>
2026-05-13 00:35:49 +0300

    Add WETHAQ beta pages

---
+++ script.js
@@ -0,0 +1,72 @@
+function login(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const emailError = document.getElementById("emailError");
+  const passwordError = document.getElementById("passwordError");
+
+  emailError.textContent = "";
+  passwordError.textContent = "";
+
+  let isValid = true;
+
+  if (email === "") {
+    emailError.textContent = "University email is required.";
+    isValid = false;
+  } else if (!email.includes("@")) {
+    emailError.textContent = "Please enter a valid university email.";
+    isValid = false;
+  }
+
+  if (password === "") {
+    passwordError.textContent = "Password is required.";
+    isValid = false;
+  } else if (password.length < 6) {
+    passwordError.textContent = "Password must be at least 6 characters.";
+    isValid = false;
+  }
+
+  if (!isValid) return;
+
+  localStorage.setItem("wethaqUserEmail", email);
+  alert("Login successful. Welcome to WETHAQ Beta!");
+
+   window.location.href = "dashboard.html";
+}
+
+function togglePassword() {
+  const passwordInput = document.getElementById("password");
+  const showButton = document.querySelector(".show-btn");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showButton.textContent = "Hide";
+  } else {
+    passwordInput.type = "password";
+    showButton.textContent = "Show";
+  }
+}
+
+function goToSubmit() {
+  window.location.href = "submit.html";
+}
+
+function goToHistory() {
+  window.location.href = "history.html";
+}
+
+function logout() {
+  localStorage.removeItem("wethaqUserEmail");
+  window.location.href = "index.html";
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  const userEmailText = document.getElementById("userEmailText");
+  const savedEmail = localStorage.getItem("wethaqUserEmail");
+
+  if (userEmailText && savedEmail) {
+    userEmailText.textContent = savedEmail;
+  }
+});
